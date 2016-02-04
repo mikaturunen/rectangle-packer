20d28f0dcab997fe3f2fca8da43d0d0b05d3f86d
Mika Turunen <mika.turunen@ymail.com>
2016-02-04 07:02:55 +0200

    Major refactor and still bonkered. Fixing later.

--- test/packer.test.js
+++ test/packer.test.js
@@ -2,51 +2,56 @@
 
 const packer = require("../lib/packer");
 
+const makeGrid = (w, h) => {
+    let array = [];
+
+    for(let i = 0; i < h; i++) {
+        array[i] = [];
+        for(let j = 0; j < w; j++) {
+            array[i][j] = "0";
+        }
+    }
+
+    return array;
+};
+
+const printGrid = (grid) => {
+    for (let y = 0; y < rectanglePacker.height; y++) {
+        let row = "";
+
+        for (let x = 0; x < rectanglePacker.width; x++) {
+            row += grid[x, y];
+        }
+
+        console.log(row);
+    }
+};
+
 const GAP = 1;
 let rectangles = [];
 
 for (let i = 0; i < 50; i++) {
-    rectangles.push({
-        x: 0,
-        y: 0,
-        w: Math.floor((Math.random() * 10) + 2),
-        h: Math.floor((Math.random() * 10) + 2),
-   });
+    rectangles.push(new packer.rectangle(0, 0, Math.floor((Math.random() * 10) + 2), Math.floor((Math.random() * 10) + 2)));
 }
 
 for (let i = 0; i < 50; i++) {
-    rectangles.push({
-        x: 0,
-        y: 0,
-        w: Math.floor((Math.random() * 10) + 7),
-        h: Math.floor((Math.random() * 4) + 1),
-   });
+    rectangles.push(new packer.rectangle(0, 0, Math.floor((Math.random() * 10) + 7), Math.floor((Math.random() * 4) + 1)));
 }
 
 for (let i = 0; i < 50; i++) {
-    rectangles.push({
-        x: 0,
-        y: 0,
-        w: Math.floor((Math.random() * 4) + 1),
-        h: Math.floor((Math.random() * 10) + 7),
-   });
+    rectangles.push(new packer.rectangle(0, 0, Math.floor((Math.random() * 4) + 1), Math.floor((Math.random() * 10) + 7)));
 }
 
 for (let i = 0; i < 50; i++) {
-    rectangles.push({
-        x: 0,
-        y: 0,
-        w: Math.floor((Math.random() * 4) + 1),
-        h: Math.floor((Math.random() * 4) + 1),
-   });
+    rectangles.push(new packer.rectangle(0, 0, Math.floor((Math.random() * 4) + 1), Math.floor((Math.random() * 4) + 1)));
 }
 
-const rectanglePacker = packer();
+const rectanglePacker = new packer.packer();
 
 for (let i = 0; i<rectangles.length; i++) {
     let rectangle = rectangles[i];
 
-    if (!rectanglePacker(rectangle, GAP, GAP)) {
+    if (!rectanglePacker.pack(rectangle, GAP)) {
         console.log("Packing failed");
         console.log("Failed for:", rectangle);
         return;
@@ -54,17 +59,11 @@ for (let i = 0; i<rectangles.length; i++) {
 
     rectangles[i] = rectangle;
 }
-console.log("Packing success");
-console.log(packer.width, rectanglePacker.width);
+console.log("Packing success:", rectanglePacker.width, rectanglePacker.height);
 
 // Create 2D grid of "pixels" and fill them where rectangles are
-let grid = [];
-for (let y = 0; y < rectanglePacker.height; y++) {
-    grid.push([]);
-    for (let x = 0; x < rectanglePacker.width; x++) {
-        grid(y).push(" ");
-    }
-}
+let grid = makeGrid(rectanglePacker.width, rectanglePacker.height);
+printGrid(grid);
 
 const totalArea = rectanglePacker.width * rectanglePacker.height;
 let usedArea = 0;
@@ -78,10 +77,6 @@ rectangles.forEach(r => {
         }
     }
 });
+printGrid(grid);
 
 console.log("totalArea:", totalArea, ", usedArea:", usedArea);
-for (let y = 0; y < rectanglePacker.height; y++) {
-    for (let x = 0; x < rectanglePacker.width; x++) {
-        console.log(grid[y, x]);
-    }
-}
